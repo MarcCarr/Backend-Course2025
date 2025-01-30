105f4cb2fb8cf3addea2fca46bd19fba23875a25
MarcCarr <bhj362@myy.haaga-helia.fi>
2025-01-30 09:51:18 +0200

    BookstoreMC restructure

--- Bookstore/bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ Bookstore/bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -2,13 +2,11 @@ package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
     @GetMapping("/index")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public String showBookstore() {
+        return "Welcome to the bookstore";
     }
-
 }
